570bed68896913c1d820fc031d4d6e41b07c5c47
Dikshita621 <dikshitachoudhary845@gmail.com>
2025-09-07 00:29:10 +0530

    Create day14(1).c

---
+++ day14(1).c
@@ -0,0 +1,10 @@
+//Q27: Write a program to print the sum of the first n odd numbers.//
+#include <stdio.h>
+
+int main() 
+{
+    int n;
+    scanf("%d", &n);
+    printf("%d\n", n * n);
+    return 0;
+}
