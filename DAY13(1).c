21384da82ae446dd32a3bdc0bdbb4bb4f7b1d796
Dikshita621 <dikshitachoudhary845@gmail.com>
2025-09-07 00:24:39 +0530

    Create DAY13(1).c

---
+++ DAY13(1).c
@@ -0,0 +1,38 @@
+//Q25: Write a program to implement a basic calculator using switch-case for +, -, *, /, %.//
+#include <stdio.h>
+
+int main() {
+    int num1, num2, result;
+    char op;
+    scanf("%d %d %c", &num1, &num2, &op);
+
+    switch (op) {
+        case '+':
+            result = num1 + num2;
+            printf("%d\n", result);
+            break;
+        case '-':
+            result = num1 - num2;
+            printf("%d\n", result);
+            break;
+        case '*':
+            result = num1 * num2;
+            printf("%d\n", result);
+            break;
+        case '/':
+            if (num2 != 0)
+                printf("%d\n", num1 / num2);
+            else
+                printf("Error: Division by zero\n");
+            break;
+        case '%':
+            if (num2 != 0)
+                printf("%d\n", num1 % num2);
+            else
+                printf("Error: Modulo by zero\n");
+            break;
+        default:
+            printf("Invalid operator\n");
+    }
+    return 0;
+}
